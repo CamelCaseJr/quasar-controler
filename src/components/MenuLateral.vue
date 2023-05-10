8e9348d312d0f54ed53882c5489eb6070a0be553
rodriguesarlj <rodriguescla2@gmail.com>
2023-05-10 15:22:01 -0300

    first commit

---
+++ src/components/MenuLateral.vue
@@ -0,0 +1,105 @@
+<template>
+
+  <q-list>
+    <q-item clickable v-ripple>
+      <q-item-section avatar>
+        <q-icon class="icon" size="md" name="fa fa-house" />
+      </q-item-section>
+      <q-item-section>Home</q-item-section>
+    </q-item>
+
+    <q-separator inset></q-separator>
+    <q-item-label header style="padding-top: 1rem"> CADASTRAR</q-item-label>
+
+    <q-item clickable v-ripple>
+      <q-item-section avatar>
+        <q-icon class="icon" name="fa fa-person-rifle" />
+      </q-item-section>
+      <q-item-section>Militar</q-item-section>
+    </q-item>
+
+    <q-item clickable v-ripple>
+      <q-item-section avatar>
+        <q-icon class="icon" name="fa fa-person-walking" />
+      </q-item-section>
+      <q-item-section>Visitante</q-item-section>
+    </q-item>
+
+    <q-item clickable v-ripple>
+      <q-item-section avatar>
+        <q-icon class="icon" name="fa fa-building" />
+      </q-item-section>
+      <q-item-section>Empresa</q-item-section>
+    </q-item>
+
+    <q-item clickable v-ripple>
+      <q-item-section avatar>
+        <q-icon class="icon" name="fa fa-building-user" />
+      </q-item-section>
+      <q-item-section>Departamentos</q-item-section>
+    </q-item>
+    <q-separator inset />
+    <q-item-label header style="padding-top: 1rem"> ACESSO</q-item-label>
+
+    <q-item clickable v-ripple>
+      <q-item-section avatar>
+        <q-icon class="icon" name="fa fa-mobile" />
+      </q-item-section>
+      <q-item-section>Dispositivos</q-item-section>
+    </q-item>
+    <q-item clickable v-ripple>
+      <q-item-section avatar>
+        <q-icon class="icon" name="fa fa-chart-area" />
+      </q-item-section>
+      <q-item-section>Áreas</q-item-section>
+    </q-item>
+    <q-item clickable v-ripple>
+      <q-item-section avatar>
+        <q-icon class="icon" name="fa fa-universal-access" />
+      </q-item-section>
+      <q-item-section>Regras de Acesso</q-item-section>
+    </q-item>
+    <q-separator inset />
+    <q-item-label header style="padding-top: 1rem"> RELATÓRIOS</q-item-label>
+
+    <q-item clickable v-ripple>
+      <q-item-section avatar>
+        <q-icon class="icon" name="fa fa-universal-access" />
+      </q-item-section>
+      <q-item-section>Acessos</q-item-section>
+    </q-item>
+    <q-item clickable v-ripple>
+      <q-item-section avatar>
+        <q-icon class="icon" name="fa fa-square-check" />
+      </q-item-section>
+      <q-item-section>Permissões</q-item-section>
+    </q-item>
+    <q-item clickable v-ripple>
+      <q-item-section avatar>
+        <q-icon class="icon" name="fa fa-address-card" />
+      </q-item-section>
+      <q-item-section>Registros</q-item-section>
+    </q-item>
+    <q-item clickable v-ripple>
+      <q-item-section avatar>
+        <q-icon class="icon" name="fa fa-bars-progress" />
+      </q-item-section>
+      <q-item-section>Status Atual</q-item-section>
+    </q-item>
+  </q-list>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MenuLateral",
+});
+</script>
+
+<style scoped lang="scss">
+$--q-icon-color: $Color-icon;
+.icon {
+  color: $--q-icon-color;
+}
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,73 @@
+<template>
+  <q-layout view="hHh lpR fFf ">
+    <q-header class="bg-grey-10">
+      <q-toolbar>
+        <q-btn
+          class="icon"
+          dense
+          flat
+          round
+          icon="menu"
+          style="padding-right: 1rem"
+          @click="toggleLeftDrawer"
+        />
+        <q-separator vertical />
+
+        <q-toolbar-title class="custom-title">
+          <q-icon class="icon" size="md" name="fa fa-globe"> </q-icon>
+          <a class="titulo">
+            <strong>Controle De Acesso</strong>
+          </a>
+        </q-toolbar-title>
+      </q-toolbar>
+      <q-separator />
+    </q-header>
+
+
+
+    <q-drawer v-model="leftDrawerOpen">
+      <MenuLateral />
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import MenuLateral from "src/components/MenuLateral.vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    MenuLateral,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
+<style scoped lang="scss">
+$--q-titulo-color: $titulo;
+$--q-icon-color: $Color-icon;
+.custom-title {
+  color: $--q-titulo-color;
+}
+.titulo {
+  padding-left: 1rem;
+}
+.icon {
+  color: $--q-icon-color;
+}
+</style>
